6b6cfd86178446306e074ca6b81a6a6af7965475
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-07-01 14:08:23 -0500

    added assets and modified ContentView

--- I Am Rich SwiftUI/ContentView.swift
+++ I Am Rich SwiftUI/ContentView.swift
@@ -10,8 +10,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold).foregroundColor(Color.white)
+                    
+                Image("diamond")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+                
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
