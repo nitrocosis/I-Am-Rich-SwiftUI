6aea79e2015e4a639eb5092e32c90350765cadf3
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-07-01 13:38:18 -0500

    Initial commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Sarah Rebecca Estrellado on 6/29/20.
+//  Copyright © 2020 Sarah Rebecca Estrellado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
